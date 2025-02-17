7d71a9620c72dfb4a64e215f47958caf545c05dd
saccomando-pietro <pietro.saccomando@gmail.com>
2025-02-17 11:39:57 +0100

    esercizio5

---
+++ esercizio5/index.php
@@ -0,0 +1,10 @@
+<?php
+$persona = [
+    "nome" => "Pietro",
+    "eta" => 19,
+    "citta" => "Verona"
+];
+
+
+echo "Mi chiamo {$persona['nome']}, ho {$persona['eta']} anni e vivo a {$persona['citta']}.";
+?>
