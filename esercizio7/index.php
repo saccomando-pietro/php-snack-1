796507b5e8d8511053d26dde1b77b3700541ca0d
saccomando-pietro <pietro.saccomando@gmail.com>
2025-02-17 11:45:19 +0100

    esercizio7

---
+++ esercizio7/index.php
@@ -0,0 +1,18 @@
+<?php
+$titolo = "titolo";
+$sottotitolo = "sottotitolo";
+$descrizione = "descrizione";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo $titolo ?></h1>
+    <h2><?php echo $sottotitolo ?></h2>
+    <p><?php echo $descrizione ?></p>
+</body>
+</html>
