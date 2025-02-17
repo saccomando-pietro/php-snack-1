a9dd67a609f179443d837dcb6d49d5ef4ccacc4b
saccomando-pietro <pietro.saccomando@gmail.com>
2025-02-17 11:52:28 +0100

    esercizio9

---
+++ esercizio9/index.php
@@ -0,0 +1,21 @@
+<?php
+$isOnline = false;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    if ($isOnline) {
+        echo '<p style="color: green;">Utente online</p>';
+    } else {
+        echo '<p style="color: red;">Utente offline</p>';
+    }
+    ?>
+</body>
+</html>
